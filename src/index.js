ca8826549dc47dbd2fc4121faf208ac25d7f6da5
Adi Bev <adityabev@gmail.com>
2018-12-30 15:38:29 +0000

    imported file paths corrected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
 import ReactDom from "react-dom";
 
-import SearchBar from "./search_bar";
+import SearchBar from "./search_bar/search_bar";
 /*styles imported here*/
-import windIconsStyles from "./styles/weather-icons-wind.min.css";
-import iconsStyles from "./styles/weather-icons.min.css";
+import windIconsStyles from "../node_modules/weathericons/css/weather-icons-wind.min.css";
+import iconsStyles from "../node_modules/weathericons/css/weather-icons.min.css";
 import style from "./styles/main.min.css";
 
 const Index = () => {
