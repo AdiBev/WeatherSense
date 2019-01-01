6df5cc02634ca9132ed11897cc56f6f8c9e1b92f
Adi Bev <adityabev@gmail.com>
2019-01-01 19:44:25 +0000

    inline style removed

--- src/error_handler/error_handler.js
+++ src/error_handler/error_handler.js
@@ -2,15 +2,10 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 export const ErrorHandler = ({ error }) => {
-  const styles = {
-    color: "#931008",
-    textAlign: "center"
-  };
-
   return (
     <Fragment>
       {error ? (
-        <div style={styles}>
+        <div id="error">
           <h2>Ooops!! {error.toString()} </h2>
           <p>Please type in your place name again.</p>
         </div>
