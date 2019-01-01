c86ac8db23f26fdd614ab6bb3eb7b2c68278082f
Adi Bev <adityabev@gmail.com>
2019-01-01 19:59:36 +0000

    toolTip for icons and miles unit added

--- src/forecast_template/forecast_windHum.js
+++ src/forecast_template/forecast_windHum.js
@@ -1,17 +1,30 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 
-export const ForecastWindHumidity = ({ windData, tempData }) => {
+export const ForecastWindHumidity = ({
+  windData,
+  tempData,
+  checked,
+  windSpeedM
+}) => {
   const windIcon = `wi wi-wind towards-${windData.deg}-deg`;
+  const windUnit = `${checked} ?  : `;
+  const windMiles = `${windSpeedM} miles/sec`;
+  const windMeter = `${Math.round(windData.speed)} m/sec`;
 
   return (
     <div id="humWind">
       <h2>
-        <i className="wi wi-strong-wind" /> {windData.speed} m/sec{" "}
-        <i className={windIcon} />
+        <i className="wi wi-strong-wind" data-tip="Wind speed" />{" "}
+        {checked ? windMiles : windMeter}{" "}
+        <i className={windIcon} data-tip="Wind direction" />
       </h2>
       <h2>
-        <i className="wi wi-humidity" /> {tempData.humidity}%
+        {/*data-tip is props for ReactTooltip*/}
+        <i className="wi wi-humidity" data-tip="Humidity" /> {tempData.humidity}
+        %
       </h2>
+      <ReactTooltip place="top" type="light" effect="float" />
     </div>
   );
 };
