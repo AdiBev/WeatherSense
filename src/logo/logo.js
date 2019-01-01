8ec81cba24fd41b4415bcc0c707f14c3f1939bc4
Adi Bev <adityabev@gmail.com>
2019-01-01 19:50:10 +0000

    new styles added for logo

--- src/logo/logo.js
+++ src/logo/logo.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-const style = {
-  textAlign: "center",
-  color: "#0a0450"
-};
-
 export const Logo = () => {
   return (
     <div id="logo">
-      <h1 style={style}>WeatherSense</h1>
+      <h1>WeatherSense</h1>
     </div>
   );
 };
